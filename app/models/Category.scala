cea9db47c391cb52653f49a446f1c1a76b1e2f68
Laura Todd <laura20todd@gmail.com>
2022-10-25 12:09:31 +0100

    Add .gitignore and cleanup

---
+++ app/models/Category.scala
@@ -0,0 +1,23 @@
+package models
+
+sealed trait Category
+
+case object Groceries extends Category {
+  override def toString: String = "Groceries"
+}
+
+case object Transport extends Category {
+  override def toString: String = "Transport"
+}
+
+case object Energy extends Category {
+  override def toString: String = "Energy"
+}
+
+case object Water extends Category {
+  override def toString: String = "Water"
+}
+
+case object Socialising extends Category {
+  override def toString: String = "Socialising"
+}
--- target/web/classes/main/META-INF/resources/webjars/heatwave/0.1/javascripts/application.js
+++
@@ -1,2 +0,0 @@
-
-/** javascripts/app.js **/
