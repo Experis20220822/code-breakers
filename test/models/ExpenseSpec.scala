627d8faac52cfc1db8f3976cb99522ccd6c69e98
Laura Todd <laura20todd@gmail.com>
2022-10-27 14:58:47 +0100

    Update tests and remove deprecated code from build.sbt

--- test/models/ExpenseSpec.scala
+++ test/models/ExpenseSpec.scala
@@ -9,14 +9,14 @@ import org.scalatestplus.play.PlaySpec
 import java.util.Date
 
 class ExpenseSpec extends PlaySpec {
-  val currentDate = new Date()
-  val expense = Expense(2L, currentDate, 10000, Groceries)
+
+  val expense = Expense("2L", Date("27", "10", "2022"), 10000, Groceries)
   "An expense" must {
     "return the expense id that I provide it" in {
-      expense.id mustBe (2L)
+      expense.id mustBe ("2L")
     }
     "return the date that I provide it" in {
-      expense.date mustBe (currentDate)
+      expense.date mustBe (Date("27", "10", "2022"))
     }
     "return the amount that I provide it" in {
       expense.amount mustBe (10000)
