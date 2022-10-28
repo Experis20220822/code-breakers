c8d08b2d6049770af9836b693df79f944f8b11d2
Laura Todd <laura20todd@gmail.com>
2022-10-28 10:16:26 +0100

    Remove unused imports

--- app/services/ExpenseService.scala
+++ app/services/ExpenseService.scala
@@ -5,14 +5,13 @@
 
 package services
 
-import models.{Date, Expense, Groceries}
+import models.Expense
 import repositories.ExpenseRepository
 
 import javax.inject.Inject
 import scala.concurrent.Future
 
 class ExpenseService @Inject() (expenseRepository: ExpenseRepository) extends AsyncExpenseService {
-  import scala.concurrent.ExecutionContext.Implicits.global
   override def create(expense: Expense): Unit = expenseRepository.create(expense)
 
   override def findById(id: String): Future[Option[Expense]] = expenseRepository.findById(id)
